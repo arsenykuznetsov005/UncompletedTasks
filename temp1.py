c22fc45438346b409a0e36591f73e86bfb58e31e
Arseny <arsenykuznetsov005@proton.me>
2025-03-13 14:16:23 +0300

    Сделал сохранение невыполненных планов в файл

--- temp1.py
+++ temp1.py
@@ -40,18 +40,10 @@ def func(current_path: str):
 
 target = getcwd()
 func(current_path=target)
-# print(dict_of_tasks)
-for file_path, task in dict_of_tasks.items():
-    if len(task) > 1:
-        print(f'Path:\n{file_path}\nTasks:')
-        # print(*task, sep='\n', end='\n\n\n')
 
-        [print(line.rstrip()) for line in task]
-        print('\n\n\n')
-
-    elif len(task) == 1:
-        print(f'Path:\n{file_path}\nTask:')
-        # print(*task, sep='\n', end='\n\n\n')
-
-        [print(line.rstrip()) for line in task]
-        print('\n\n\n')
+with open(file='tasks.md', mode='a', encoding='utf-8') as file:
+    for file_path, task in dict_of_tasks.items():
+        file.write(f'Путь: `{file_path}`\nПланы:\n')
+        for _ in task:
+            file.write(_)
+        file.write('\n\n\n')
